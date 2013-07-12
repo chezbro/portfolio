0bdcc4b823e5eaa832921cbb5505e7bff28a1a09
Eric Chesbrough <EricChesbrough@gmail.com>
2013-07-12 12:28:02 -0400

    more ui stuff

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,21 @@
+// Smint
+
 $(document).ready( function() {
     $('.subMenu').smint({
     	'scrollSpeed' : 1200
     });
 });
 
+// Back to Top
+
+$(function() {
+  $.scrollUp({
+    scrollName: 'scrollUp', // Element ID
+    topDistance: '300', // Distance from top before showing element (px)
+    topSpeed: 300, // Speed back to top (ms)
+    animation: 'fade', // Fade, slide, none
+    animationInSpeed: 400, // Animation in speed (ms)
+    animationOutSpeed: 400, // Animation out speed (ms)
+    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+  });
+});
